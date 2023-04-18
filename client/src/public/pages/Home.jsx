d86c744c10e5db9a18528779a2244473ebd33458
akash <akash@gmail.com>
2023-04-18 10:34:44 +0530

    18-04-2023

--- client/src/public/pages/Home.jsx
+++ client/src/public/pages/Home.jsx
@@ -1,9 +1,11 @@
 import React from 'react'
 import Hero from '../components/Hero'
+import Categories from '../components/Categories'
 
 const Home = () => {
     return <>
         <Hero />
+        <Categories />
     </>
 }
 
