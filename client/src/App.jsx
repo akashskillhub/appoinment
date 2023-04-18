374ff66f3c479be28ee50111e36ea2a7c956fe9a
akash <akash@gmail.com>
2023-04-18 10:16:13 +0530

    17-04-2023

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,9 +1,18 @@
 import React from 'react'
-
+import { BrowserRouter, Route, Routes } from "react-router-dom"
+import PublicLayout from './public/PublicLayout'
+import Home from './public/pages/Home'
 const App = () => {
-  return (
-    <div>App</div>
-  )
+  return <>
+    < BrowserRouter>
+      <Routes>
+        <Route path="/" element={<PublicLayout />}>
+          <Route index element={<Home />} />
+        </Route>
+      </Routes>
+    </ BrowserRouter>
+
+  </>
 }
 
 export default App
